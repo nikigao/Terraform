2dbbd8fde1b55c47d471f9e25f39ae84d65a10ea
nikigao <nikigao1@gmail.com>
2020-04-12 13:41:14 -0400

    changes

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -14,10 +14,7 @@ variable "AMIS" {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-00890f614e48ce866"
-<<<<<<< HEAD
-    eu-west-2 = "ami-00100"
-=======
->>>>>>> a72a015252767d076f7991eb2910663eb842ce30
+
   }
 }
 
